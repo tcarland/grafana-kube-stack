66a6d5a6a2be9cf414239c8711ed44ec26ce6de5
Timothy C. Arland <tcarland@gmail.com>
2025-04-24 04:46:43 -0600

    initial commit

---
+++ kube-stack-setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+#
+PNAME=${0##*\/}
+VERSION="v25.04.25"
+
+binpath=$(dirname "$0")
+project=$(dirname "$(realpath "$binpath")")
+
+export S3_BUCKET_NAME=${S3_BUCKET_NAME:-"mimir/data"}
+export S3_SKIP_VERIFY=${MINIO_TLS_INSECURE:-"true"}
+
+# strip protocol from endpoint
+
+
+s3_secrets="
+S3_REGION=\${S3_REGION}
+S3_ENDPOINT=\${S3_ENDPOINT}
+S3_BUCKET_NAME=\${S3_BUCKET_NAME
+S3_ACCESS_KEY=\${S3_ACCESS_KEY}
+S3_SECRET_KEY=\${S3_SECRET_KEY}
+S3_SKIP_VERIFY=\${S3_SKIP_VERIFY}
+"
+
+# validation checks
+if ! which helm >/dev/null 2>&1; then
+    echo "$PNAME Error, required binary 'helm' not found in PATH." >&2
+    exit 2
+fi
+
+if [ -z "$S3_ENDPOINT" ]; then
+    echo "$PNAME Error, S3_ENDPOINT not defined." >&2
+    exit 1
+fi
+
+if [[ -z "$S3_ACCESS_KEY" || -z "$S3_SECRET_KEY" ]]; then
+    echo "$PNAME Error, S3 credentials not defined." >&2
+    exit 1
+fi
+
+
+( echo "$s3_secrets" | envsubst > mimir/base/secrets.env )
